f242e5b11683e598ff31dce04b68113b1d3c560c
Ruben Vitt <dev@rubeen.dev>
2020-09-17 09:33:36 +0200

    :sparkles:(index): some implementations for index-page

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import {LayoutComponent} from "../components/layout.component";
 
 export default function Home() {
   return (
-      <LayoutComponent />
+      <LayoutComponent header={'Ruben Vitt'}>
+        This is my content
+      </LayoutComponent>
   )
 }
